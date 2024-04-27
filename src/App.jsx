8c2316dfb01ba4d0a1334877503826bdf13b46f1
Saranya2024 <159130151+Saranya2024@users.noreply.github.com>
2024-04-27 16:52:20 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <div className="counter">{counter}</div>
+      <div className="buttons">
+        <button onClick={increaseCounter}>+</button>
+        <button onClick={decreaseCounter}>-</button>
+        <button onClick={resetCounter}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
